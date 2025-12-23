27898b9d6526fad486535685d21068ef08e35a7d
MicroSaasCreatons-afk <cvdyer@gmail.com.com>
2025-12-23 14:24:25 -0500

    Add Next.js required files: page.js, layout.js, globals.css

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>MicroSaaS Creations</h1>
+      <p>Voice attendant demo for (478) 777-5451</p>
+      <p>Call now: (478) 777-5451</p>
+    </div>
+  );
+}
